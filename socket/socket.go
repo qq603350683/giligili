9b84bc31b2c49a77f0617ae86dbb987bc042d031
LooZzie <603350683@qq.com>
2020-08-13 18:32:33 +0800

    Update socket.go

--- socket/socket.go
+++ socket/socket.go
@@ -7,7 +7,7 @@ import (
 )
 
 type WebsocketPool struct {
-	num int
+	num chan int
 	connects map[string]*websocket.Conn
 }
 
@@ -18,10 +18,23 @@ var upgrade = websocket.Upgrader{
 	},
 }
 
+var WebsocketConns *WebsocketPool
+
+func NewWebsocketPool() *WebsocketPool {
+	pool := &WebsocketPool{
+		num: make(chan int, 1),
+		connects: make(map[string]*websocket.Conn),
+	}
+
+	return pool
+}
+
 func main() {
 	//http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 	//	w.Write([]byte("hello world"))
 	//})
+
+	WebsocketConns = NewWebsocketPool()
 
 	http.HandleFunc("/qqq", func (w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrade.Upgrade(w, r, nil)
@@ -45,12 +58,24 @@ func main() {
 				return
 			}
 
-			fmt.Println(token)
+			WebsocketConns.num <- 1
+			WebsocketConns.connects[token] = conn
+
 			for {
+				fmt.Println(WebsocketConns.num)
 				mtype, content, err := conn.ReadMessage()
 				if err != nil {
 					fmt.Println("客户端主动关闭链接")
 					conn.Close()
+
+					if _, ok := <- WebsocketConns.num; !ok {
+						fmt.Println("WebsocketConns.num - 1 fail")
+						return
+					}
+
+					fmt.Println("玩家数剩余")
+					fmt.Println(len(WebsocketConns.num))
+
 					return
 				}
 				switch mtype {
@@ -68,6 +93,8 @@ func main() {
 					// PongMessage
 				}
 			}
+
+			// 永远都不会执行到这里
 		}(token[0], conn)
 	})
 
