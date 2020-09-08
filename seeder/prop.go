32004b197d9d02ced3b0a1055079d101f491ab92
luojie <qq603350683@gmail.com>
2020-09-08 21:49:59 +0800

    修改部分文字...

--- seeder/prop.go
+++ seeder/prop.go
@@ -22,7 +22,7 @@ func PropRun() {
 	props = append(props, model.Prop{
 		PID:       1,
 		Type:      "gold",
-		Image:     "prop_gold.png",
+		Image:     "prop/gold.png",
 		Title:     "金币大礼包",
 		Remark:    "金币大礼包",
 		CreatedAt: time.Now(),
@@ -31,7 +31,7 @@ func PropRun() {
 	props = append(props, model.Prop{
 		PID:       2,
 		Type:      "diamond",
-		Image:     "prop_diamond.png",
+		Image:     "prop/diamond.png",
 		Title:     "钻石大礼包",
 		Remark:    "钻石大礼包",
 		CreatedAt: time.Now(),
@@ -40,7 +40,7 @@ func PropRun() {
 	props = append(props, model.Prop{
 		PID:       3,
 		Type:      "stone_enhancer_material",
-		Image:     "prop_stone_enhancer_material.png",
+		Image:     "prop/stone_enhancer_material.png",
 		Title:     "石岩碳",
 		Remark:    "强化子弹、技能攻击力时所需要的石头",
 		CreatedAt: time.Now(),
@@ -49,7 +49,7 @@ func PropRun() {
 	props = append(props, model.Prop{
 		PID:       4,
 		Type:      "stone_speed_enhancer_material",
-		Image:     "prop_stone_speed_enhancer_material.png",
+		Image:     "prop/stone_speed_enhancer_material.png",
 		Title:     "竹碳",
 		Remark:    "强化子弹、技能速度时所需要的石头",
 		CreatedAt: time.Now(),
@@ -58,36 +58,36 @@ func PropRun() {
 	props = append(props, model.Prop{
 		PID:       1001,
 		Type:      "bullet_enhancer",
-		Image:     "prop_bullet_enhancer.png",
-		Title:     "子弹攻击力强化器",
-		Remark:    "子弹攻击力强化器",
+		Image:     "prop/bullet_enhancer.png",
+		Title:     "B攻击强化器",
+		Remark:    "B攻击强化器",
 		CreatedAt: time.Now(),
 	})
 
 	props = append(props, model.Prop{
 		PID:       1002,
 		Type:      "bullet_speed_enhancer",
-		Image:     "prop_bullet_speed_enhancer.png",
-		Title:     "子弹速度强化器",
-		Remark:    "子弹速度强化器",
+		Image:     "prop/bullet_speed_enhancer.png",
+		Title:     "B速度强化器",
+		Remark:    "B速度强化器",
 		CreatedAt: time.Now(),
 	})
 
 	props = append(props, model.Prop{
 		PID:       1003,
 		Type:      "skill_enhancer",
-		Image:     "prop_skill_enhancer.png",
-		Title:     "技能攻击力强化器",
-		Remark:    "技能攻击力强化器",
+		Image:     "prop/skill_enhancer.png",
+		Title:     "S攻击强化器",
+		Remark:    "S攻击强化器",
 		CreatedAt: time.Now(),
 	})
 
 	props = append(props, model.Prop{
 		PID:       1004,
 		Type:      "skill_speed_enhancer",
-		Image:     "prop_skill_speed_enhancer.png",
-		Title:     "技能速度强化器",
-		Remark:    "技能速度强化器",
+		Image:     "prop/skill_speed_enhancer.png",
+		Title:     "S速度强化器",
+		Remark:    "S速度强化器",
 		CreatedAt: time.Now(),
 	})
 
