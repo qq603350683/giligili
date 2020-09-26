0e9ffa8d613374df11800ce518241ed81cfe6659
luojie <qq603350683@gmail.com>
2020-09-26 23:03:25 +0800

    ....

--- seeder/plan.go
+++ seeder/plan.go
@@ -20,59 +20,39 @@ func PlanRun() {
 		return
 	}
 
+	//plans = append(plans, model.Plan{
+	//	PID:        1,
+	//	DetailJson: `{"name":"初代战神","w":129,"h":120,"texture":"hero/hero2.png","bullets":[{"id":1,"title":"A导弹","w":25,"h":52,"p":3,"a":0,"level":1,"max_level":3,"rate":20,"max_rate":18,"speed":11,"max_speed":15,"texture":"bullet/10.png"}],"skills":[{"id":1,"title":"光速射线","w":50,"h":50,"p":1,"a":0,"level":2,"max_level":4,"rate":180,"max_rate":175,"speed":50,"max_speed":60,"height":9999999,"texture":"bullet/skill1.png"}]}`,
+	//	DelAt:      model.DelAtDefaultTime,
+	//	CreatedAt:  time.Now(),
+	//})
+
 	plans = append(plans, model.Plan{
 		PID:        1,
-		DetailJson: `{
-	"name": "FG-001",
-	"w":129,
-	"h":120,
-	"texture":"hero/hero2.png",
-	"bullets":[
-		{"id":1,"w":20,"h":20,"p":3,"a":0,"level":1,"max_level":3,"rate":180,"max_rate":175,"speed":10,"max_speed":15,"texture":"bullet/10.png"}
-	],
-	"skills":[
-		{"id":1,"w":50,"h":50,"p":3,"a":0,"level":2,"max_level":12,"rate":300,"max_rate":290,"speed":50,"max_speed":45,"height":9999999,"texture":"bullet/skill1.png"}
-	]
-}`,
+		DetailJson: `{"name":"2020飞行机","w":129,"h":120,"texture":"hero/hero2.png","bullets":[{"id":1,"title":"2020导弹","w":25,"h":52,"p":1,"a":0,"level":1,"max_level":3,"rate":30,"max_rate":25,"speed":10,"max_speed":15,"texture":"bullet/10.png"}, {"id":2,"title":"2020导弹","w":25,"h":52,"p":3,"a":0,"level":1,"max_level":3,"rate":30,"max_rate":25,"speed":10,"max_speed":15,"texture":"bullet/10.png"}, {"id":3,"title":"2020导弹","w":25,"h":52,"p":5,"a":0,"level":1,"max_level":3,"rate":30,"max_rate":25,"speed":10,"max_speed":15,"texture":"bullet/10.png"}],"skills":[{"id":1,"title":"2020射线","w":50,"h":50,"p":1,"a":0,"level":2,"max_level":4,"rate":180,"max_rate":175,"speed":50,"max_speed":60,"height":9999999,"texture":"bullet/skill1.png"}]}`,
 		DelAt:      model.DelAtDefaultTime,
 		CreatedAt:  time.Now(),
 	})
 
-	plans = append(plans, model.Plan{
-		PID:        2,
-		DetailJson: `{
-	"name": "FG-002",
-	"w":129,
-	"h":120,
-	"texture":"hero/hero2.png",
-	"bullets":[
-		{"id":1,"w":20,"h":20,"p":3,"a":0,"level":2,"max_level":4,"rate":160,"max_rate":155,"speed":10,"max_speed":15,"texture":"bullet/10.png"}
-	],
-	"skills":[
-		{"id":1,"w":50,"h":50,"p":3,"a":0,"level":2,"max_level":12,"rate":250,"max_rate":240,"speed":50,"max_speed":50,"height":9999999,"texture":"bullet/skill1.png"}
-	]
-}`,
-		DelAt:      model.DelAtDefaultTime,
-		CreatedAt:  time.Now(),
-	})
 
-	plans = append(plans, model.Plan{
-		PID:        3,
-		DetailJson: `{
-	"name": "FG-003",
-	"w":129,
-	"h":120,
-	"texture":"hero/hero2.png",
-	"bullets":[
-		{"id":1,"w":20,"h":20,"p":3,"a":0,"level":2,"max_level":8,"rate":50,"max_rate":40,"speed":15,"max_speed":25,"texture":"bullet/10.png"}
-	],
-	"skills":[
-		{"id":1,"w":50,"h":50,"p":3,"a":0,"level":3,"max_level":20,"rate":180,"max_rate":170,"speed":50,"max_speed":50,"height":9999999,"texture":"bullet/skill1.png"}
-	]
-}`,
-		DelAt:      model.DelAtDefaultTime,
-		CreatedAt:  time.Now(),
-	})
+//
+//	plans = append(plans, model.Plan{
+//		PID:        3,
+//		DetailJson: `{
+//	"name": "FG-003",
+//	"w":129,
+//	"h":120,
+//	"texture":"hero/hero2.png",
+//	"bullets":[
+//		{"id":1,"w":20,"h":20,"p":3,"a":0,"level":2,"max_level":8,"rate":50,"max_rate":40,"speed":15,"max_speed":25,"texture":"bullet/10.png"}
+//	],
+//	"skills":[
+//		{"id":1,"w":50,"h":50,"p":3,"a":0,"level":3,"max_level":20,"rate":180,"max_rate":170,"speed":50,"max_speed":50,"height":9999999,"texture":"bullet/skill1.png"}
+//	]
+//}`,
+//		DelAt:      model.DelAtDefaultTime,
+//		CreatedAt:  time.Now(),
+//	})
 
 	for _, plan := range(plans) {
 		plan.DetailJson = strings.Replace(plan.DetailJson, " ", "", -1)
