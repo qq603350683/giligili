92ecfe7207a8ddf19af4fb89e99c06c9b8dc179f
luojie <qq603350683@gmail.com>
2020-11-16 22:27:53 +0800

    优化UI

--- socket/api.go
+++ socket/api.go
@@ -8,6 +8,10 @@ func Api() {
 	AddRoute("user", HandlerFunc(GetUserInfo))
 	AddRoute("user/plan/change", HandlerFunc(UserPlanChange))
 
+	AddRoute("user/plan/upgrade/bullets/level", HandlerFunc(UserPlanUpgradeBulletsLevel))
+	AddRoute("user/plan/upgrade/bullets/speed", HandlerFunc(UserPlanUpgradeBulletsSpeed))
+	AddRoute("user/plan/upgrade/bullets/rate", HandlerFunc(UserPlanUpgradeBulletsRate))
+
 	AddRoute("user/plan/upgrade/bullet/level", HandlerFunc(UserPlanUpgradeBulletLevel))
 	AddRoute("user/plan/upgrade/bullet/speed", HandlerFunc(UserPlanUpgradeBulletSpeed))
 	AddRoute("user/plan/upgrade/bullet/rate", HandlerFunc(UserPlanUpgradeBulletRate))
@@ -15,6 +19,10 @@ func Api() {
 	AddRoute("user/plan/upgrade/skill/level", HandlerFunc(UserPlanUpgradeSkillLevel))
 	AddRoute("user/plan/upgrade/skill/speed", HandlerFunc(UserPlanUpgradeSkillSpeed))
 	AddRoute("user/plan/upgrade/skill/rate", HandlerFunc(UserPlanUpgradeSkillRate))
+
+	AddRoute("user/plan/upgrade/skills/level", HandlerFunc(UserPlanUpgradeSkillsLevel))
+	AddRoute("user/plan/upgrade/skills/speed", HandlerFunc(UserPlanUpgradeSkillsSpeed))
+	AddRoute("user/plan/upgrade/skills/rate", HandlerFunc(UserPlanUpgradeSkillsRate))
 
 	AddRoute("backpack/get-list", HandlerFunc(GetBackpackList))
 	AddRoute("backpack/prop/use", HandlerFunc(BackpackPropUse))
---
+++ socket/user_plan_upgrade_skills_speed.go
@@ -0,0 +1,88 @@
+package socket
+
+import (
+	"encoding/json"
+	"fmt"
+	"giligili/constbase"
+	"giligili/model"
+	"giligili/serializer"
+	"giligili/util"
+	"log"
+	"net/http"
+)
+
+func UserPlanUpgradeSkillsSpeed(params Params) {
+	up_id := 0
+	speed := 0
+
+	if _, ok := params["up_id"]; !ok {
+		SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, "参数异常", nil, "up_id 为 0"))
+		return
+	}
+
+	up_id = util.StringToInt(params["up_id"])
+
+	user_plan := model.GetUserPlanInfo(up_id)
+	if user_plan == nil {
+		SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, "升级失败", nil, fmt.Sprintf("当前up_id(%d)不存在", up_id)))
+		return
+	}
+
+	for i, b := range(user_plan.Detail.Skills) {
+		if speed == 0 {
+			speed = b.Speed
+
+			if b.Speed >= b.MaxSpeed {
+				SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, "速度已达最大等级", nil, ""))
+				return
+			}
+		}
+
+		b.Speed += 1
+
+		user_plan.Detail.Skills[i] = b
+	}
+
+	// 消耗的金币
+	consume_gold := speed * 100
+
+	if model.UserInfo.Gold < consume_gold {
+		SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, fmt.Sprintf("金币不足 %d", consume_gold), nil, "金币不足"))
+		return
+	}
+
+	db := model.DBBegin()
+
+	str, err := json.Marshal(user_plan.Detail)
+	if err != nil {
+		db.Rollback()
+		log.Println(err.Error())
+		SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, "升级失败", nil, err.Error()))
+		return
+	}
+
+	user_plan.DetailJson = string(str)
+
+	err = db.Save(user_plan).Error
+	if err != nil {
+		db.Rollback()
+		log.Println(err.Error())
+		SendMessage(model.UserInfo.UID, serializer.JsonByte(http.StatusInternalServerError, "升级失败", nil, err.Error()))
+		return
+	}
+
+	model.UserInfo.GoldAndDiamondUpdate(-consume_gold, 0)
+
+	db.Commit()
+
+	if model.UserInfo.UpID == up_id {
+		model.UserInfo.Plan = user_plan
+	}
+
+	user_plan_upgrade := NewUserPlanUpgrade()
+	user_plan_upgrade.Gold = model.UserInfo.Gold
+	user_plan_upgrade.Diamond = model.UserInfo.Diamond
+	user_plan_upgrade.UserPlan = user_plan
+
+	SendMessage(model.UserInfo.UID, serializer.JsonByte(constbase.USER_PLAN_UPGRADE_SUCCESS, "升级成功", user_plan_upgrade, ""))
+}
